e64f36ad9c0d08b4ef4273c1f7f472f2e7dbae6e
htrung <delta199502@gmail.com>
2023-03-20 17:56:04 +0200

    update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,27 +23,8 @@ module.exports = {
       "error",
         2
       ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "never"
-    ],
-    "eqeqeq": "error",
-    "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
-    ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
-    "no-console": "error",
+   
+    "no-console": 0,
     "react/prop-types": 0
   }
 }
